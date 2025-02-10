794c17fa117fa22af445285e718ad144b0a52b60
manees <singhmanish5208@gmail.com>
2025-02-10 00:31:06 -0600

    configured the desired route

--- bustrack.py
+++ bustrack.py
@@ -6,5 +6,17 @@ system= passiogo.getSystemFromID(system_id)
 
 
 routes= system.getRoutes()
-print(routes)
+
+
+
+for i in range(42):
+    print(routes[i].__dict__['shortName'],i)
+
+
+# print(routes[11].__dict__)
+
+# route id upto 42
+ 
+#for bus 11, route id is 22, 28, 
+#for bus 12, route id is 32, 37
 
